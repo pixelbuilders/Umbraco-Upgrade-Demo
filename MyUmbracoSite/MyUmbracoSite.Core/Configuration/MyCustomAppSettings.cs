50eed9fc30ab2041b9c149918a819b6842b850a3
Andy Boot <andyboot@pixelbuilders.com>
2025-03-23 15:03:21 +0000

    Umbraco 8 site commits

---
+++ MyUmbracoSite/MyUmbracoSite.Core/Configuration/MyCustomAppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace MyUmbracoSite.Core.Configuration
+{
+    public class MyCustomAppSettings
+    {
+        public MyCustomAppSettings()
+        {
+            TomorrowIoApiKey = ConfigurationManager.AppSettings["Tomorrow.io.ApiKey"];
+            TomorrowIoLocationName = ConfigurationManager.AppSettings["Tomorrow.io.Location.Name"];
+            TomorrowIoLocationLatitude = ConfigurationManager.AppSettings["Tomorrow.io.Location.Latitude"];
+            TomorrowIoLocationLongitude = ConfigurationManager.AppSettings["Tomorrow.io.Location.Longitude"];
+        }
+
+        public string TomorrowIoApiKey { get; set; }
+        public string TomorrowIoLocationName { get; set; }
+        public string TomorrowIoLocationLatitude { get; set; }
+        public string TomorrowIoLocationLongitude { get; set; }
+    }
+}
---
+++ MyUmbracoSite/MyUmbracoSite.Core/Models/Extended/HomePage.cs
@@ -0,0 +1,11 @@
+﻿using MyUmbracoSite.Core.Models.Weather;
+using System.Collections.Generic;
+
+namespace Umbraco.Web.PublishedModels
+{
+    public partial class HomePage
+    {
+        public string WeatherLocation { get; set; }
+        public List<WeatherDay> WeatherForecast { get; set; }
+    }
+}
